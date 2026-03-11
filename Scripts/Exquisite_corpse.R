71a745fdf0a8953e839194cfb34748f7088de461
JAAubin <jaclyn.a.aubin@gmail.com>
2026-03-11 13:41:31 -0400

    added more details to the instructions

--- Scripts/Exquisite_corpse.R
+++ Scripts/Exquisite_corpse.R
@@ -7,13 +7,14 @@
 #We are writing a collaborative story!
 #each person writes three sentences, continuing from the previous person's last sentence
 #Use hashtags to indicate that you are writing text, not code
-#Hide your first two sentences using "===="
+#Collapse your first two sentences using "===="
+#This ensures that the following writers see only the last sentence that you wrote
 
 #Example
 #====
 #There was a door at the end of the street that had never been there before.
-#Marla was certain that if she went through the door, she would have a difficult time getting home.
+#She was certain that if she went through the door, she would have a difficult time getting home.
 #====
-#One afternoon, overcome with curiosity, she stepped through the mysterious door.
+#One afternoon, overcome with curiosity, Marla stepped through the mysterious door.
 
 #====
