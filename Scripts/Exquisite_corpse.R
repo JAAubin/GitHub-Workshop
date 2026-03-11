3442835f7b684f499ef892c613421c4d51c5dfcf
JAAubin <jaclyn.a.aubin@gmail.com>
2026-03-11 11:56:26 -0400

    added an example of an exquisite corpse

--- Scripts/Exquisite_corpse.R
+++ Scripts/Exquisite_corpse.R
@@ -3,3 +3,18 @@
 #by the Mennill lab
 #started 11 March 2026
 
+##Instructions: 
+#We are writing a collaborative story
+#each person writes three sentences, continuing from the previous person's last sentence
+#Use hashtags to indicate that you are writing text, not code
+#Hide your first two sentences using "===="
+
+#Example
+
+#====
+#There was a door at the end of the street that had never been there before.
+#Marla was certain that if she went through the door, she would have a difficult time getting home.
+#====
+#One afternoon, overcome with curiosity, she stepped through the mysterious door.
+
+#====
