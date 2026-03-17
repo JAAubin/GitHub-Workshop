84ae1fa39d8458b33924518a8f6465629c599553
JAAubin <jaclyn.a.aubin@gmail.com>
2026-03-17 12:06:00 -0400

    modified instructions

--- Scripts/Exquisite_corpse.R
+++ Scripts/Exquisite_corpse.R
@@ -1,26 +1,12 @@
 ####Exquisite Corpse
 
 #by the Mennill lab
-#started 11 March 2026
+#started 19 March 2026
 
 ##Instructions: 
 #We are writing a collaborative story!
+#Open a word document
 #each person writes three sentences, continuing from the previous person's last sentence
-#Use hashtags to indicate that you are writing text, not code
-#Collapse your first two sentences using "===="
-#This ensures that the following writers see only the last sentence that you wrote
+#Only paste your last sentence into this script
 
-#Example
-#====
-#There was a door at the end of the street that had never been there before.
-#She was certain that if she went through the door, she would have a difficult time getting home.
-#====
-#One afternoon, overcome with curiosity, Marla stepped through the mysterious door.
-
-#====
-#The room inside was dimly lit with UV lights.
-#Marla found this made her eyes sting, it was very uncomfortable.
-
-#====
-#
-#Marla shrieked in discomfort, ran out of the room slamming the door behind her, and exited the building to stare into the sun.
+#Jaclyn
